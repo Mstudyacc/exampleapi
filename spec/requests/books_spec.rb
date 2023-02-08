be3e028e47e99344001749c7accedd6a804e27b9
Max <maximzakharov5@gmail.com>
2023-02-08 15:09:55 +0300

    Add delete method test using let!, and add before do block before the executing the first tets

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -2,11 +2,13 @@ require 'rails_helper'
 
 describe 'Books API', type: :request do
   describe 'GET /books' do 
-    it 'returns all books' do
+    before do 
       #Создаём 2 объекта класса book in the test db using factory rails bot gem
       FactoryBot.create(:book, title: "1994", author: "George Orwell")
       FactoryBot.create(:book, title: "RoR", author: "DHH")
+    end
 
+    it 'returns all books' do
       get '/api/v1/books'
       
       #Мы ожидаем что гет запрос по адресу выше заврешиться успешно (код 200)
@@ -24,6 +26,18 @@ describe 'Books API', type: :request do
       }.to change {Book.count}.from(0).to(1)
       
       expect(response).to have_http_status(:created)
+    end
+  end
+
+  describe 'DELETE /books/id' do
+    let!(:book) { FactoryBot.create(:book, title: "1994", author: "George Orwell") }
+
+    it 'deletes a book' do 
+      expect {
+        delete "/api/v1/books/#{book.id}"
+      }.to change {Book.count}.from(1).to(0)
+      
+      expect(response).to have_http_status(:no_content)
     end
   end
 end
